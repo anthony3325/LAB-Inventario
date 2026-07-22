a56981800aa792988b8477286a7f1792948d71a4
anthony3325 <anthonycuenca288@gmail.com>
2026-07-21 20:23:31 -0500

    Paquete Principal

--- LabInventario/src/Principal/LabInventario.java
+++ LabInventario/src/Principal/LabInventario.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Principal;
 
-/**
- *
- * @author ashle
- */
+import Controlador.ActivoDAO;
+import Controlador.IActivoRepository;
+import Controlador.MantenimientoService;
+import Vista.VistaInventario;
+
 public class LabInventario {
-    
+
+    public static void main(String[] args) {
+        IActivoRepository repositorio = new ActivoDAO();
+        MantenimientoService servicio = new MantenimientoService(repositorio);
+        new VistaInventario(servicio).menu();
+    }
 }
